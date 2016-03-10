872268889f44a81d65cfa5db825cbcc859e50d88
Zac Colley <hi@zaccolley.com>
2016-03-10 19:36:14 +0000

    DAMN

--- index.js
+++ index.js
@@ -13,18 +13,57 @@ var streaming = false;
 // The various HTML elements we need to configure or control. These
 // will be set by the startup() function.
 
-var video = null;
-var canvas = null;
-var photo = null;
-var startbutton = null;
+var video;
+var canvas;
+var photo;
+
+var takenPicture = false;
+
+function getUserMedia(constraints){
+  return new Promise(function(resolve, reject) {
+    var promisifiedOldGUM = function(constraints, successCallback, errorCallback) {
+      // first get ahold of getUserMedia, if present
+      var getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+
+      // some browsers just don't implement it - return a rejected promise with an error to keep a consistent interface
+      if (!getUserMedia) {
+        return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+      }
+
+      // convert new contraints to old
+      constraints.video = !!constraints.video;
+
+      // otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+      return new Promise(function(successCallback, errorCallback) {
+        getUserMedia.call(navigator, constraints, successCallback, errorCallback);
+      });
+    }
+
+    // older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+      navigator.mediaDevices = {};
+    }
+
+    // some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = promisifiedOldGUM;
+    }
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then(resolve)
+      .catch(reject);
+  });
+};
 
 function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+  var hex = c.toString(16);
+  return hex.length == 1 ? '0' + hex : hex;
 }
 
 function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+  return '#' + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
 // Fill the photo with an indication that none has been
@@ -32,49 +71,11 @@ function rgbToHex(r, g, b) {
 
 function clearphoto() {
   var context = canvas.getContext('2d');
-  context.fillStyle = "#AAA";
+  context.fillStyle = '#AAA';
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   var data = canvas.toDataURL('image/png');
   photo.setAttribute('src', data);
-
-  var image = photo;
-  image.onload = function(){
-    var colorThief = new ColorThief();
-    var rgbColor = colorThief.getColor(image);
-    var hexColor = rgbToHex(rgbColor[0], rgbColor[1], rgbColor[2]);
-
-    var colorInfo = ntc.name(hexColor);
-    var colorOut = colorInfo[0];
-    var colorName = colorInfo[1];
-
-    var voice = 'ukenglishfemale2',
-       speed = 0,
-       pitch = 0;
-
-    var apiKey = '34b06ef0ba220c09a817fe7924575123';
-
-    var url = 'https://api.ispeech.org/api/rest.mp3' +
-             '?apikey=' + apiKey +
-             '&action=convert' +
-             '&voice=' + voice +
-             '&speed=' + speed +
-             '&pitch=' + pitch +
-             '&text=' + colorName + ' coloured clothing item.';
-
-    var outputColor = document.querySelector('.outputColor');
-    var startOutput = document.querySelector('#startAudio');
-    var audioOutput = document.querySelector('#outputAudio');
-    outputColor.style.background = colorOut;
-    outputColor.innerHTML = colorName;
-    audioOutput.src = url;
-    startOutput.play();
-
-    startOutput.addEventListener('ended', function(){
-      audioOutput.play();
-    });
-  };
-
 }
 
 // Capture a photo by fetching the current contents of the video
@@ -90,6 +91,7 @@ function takepicture() {
     canvas.height = height;
     context.drawImage(video, 0, 0, width, height);
 
+    takenPicture = true; // first time we take the picture set this to true
     var data = canvas.toDataURL('image/png');
     photo.setAttribute('src', data);
   } else {
@@ -97,40 +99,107 @@ function takepicture() {
   }
 }
 
+function getTextToSpeechUrl(text) {
+  var voice = 'usenglishmale2',
+     speed = 0,
+     pitch = 0;
+
+  var apiKey = '34b06ef0ba220c09a817fe7924575123';
+
+  var url = 'https://api.ispeech.org/api/rest.mp3' +
+           '?apikey=' + apiKey +
+           '&action=convert' +
+           '&voice=' + voice +
+           '&speed=' + speed +
+           '&pitch=' + pitch +
+           '&text=' + text;
+
+   return url;
+}
+
 function startup() {
   video = document.getElementById('video');
   canvas = document.getElementById('canvas');
   photo = document.getElementById('photo');
-  startbutton = document.getElementById('startbutton');
+  var startButtonEl = document.querySelector('.start-button');
 
   navigator.getMedia = ( navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
                          navigator.mozGetUserMedia ||
                          navigator.msGetUserMedia);
 
-  navigator.getMedia({ video: true, audio: false }, function(stream) {
-      if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-      } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.src = vendorURL.createObjectURL(stream);
-      }
-      video.play();
-    },
-    function(err) {
-      console.log("An error occured! " + err);
-    }
-  );
 
-  video.addEventListener('canplay', function(ev){
+  var splashEl = document.querySelector('.splash');
+
+  var outputColor = document.querySelector('.outputColor');
+
+  var splashAudio = document.querySelector('#splashAudio');
+  var startOutput = document.querySelector('#startAudio');
+  var audioOutput = document.querySelector('#outputAudio');
+
+  startButtonEl.addEventListener('click', function(event) {
+    console.log('start button pressed');
+    splashAudio.play();
+    damnnn();
+
+    splashEl.classList.add('splash--hidden');
+    setTimeout(function() {
+      splashEl.style.display = 'none';
+    }, 1000);
+  });
+
+  startOutput.volume = 1;
+  audioOutput.volume = .5;
+
+  startOutput.addEventListener('ended', function(){
+    audioOutput.play();
+  });
+
+  photo.addEventListener('load', function() {
+    // only process image if we've taken a picture
+    if (takenPicture) {
+      var colorThief = new ColorThief();
+      var rgbColor = colorThief.getColor(photo);
+      var hexColor = rgbToHex(rgbColor[0], rgbColor[1], rgbColor[2]);
+
+      var colorInfo = ntc.name(hexColor);
+      var colorOut = colorInfo[0];
+      var colorName = colorInfo[1];
+
+      outputColor.style.background = colorOut;
+      outputColor.innerHTML = colorName;
+      audioOutput.src = getTextToSpeechUrl(colorName + ' coloured clothing item.');
+
+      startOutput.play();
+    }
+  });
+}
+
+function damnnn() {
+  getUserMedia({ audio: false, video: { facingMode: { exact: 'environment' } } })
+  .then(function(stream) {
+    if (navigator.mozGetUserMedia) {
+      video.mozSrcObject = stream;
+    } else {
+      var vendorURL = window.URL || window.webkitURL;
+      video.src = vendorURL.createObjectURL(stream);
+    }
+
+    video.play();
+  })
+  .catch(function(error) {
+    console.log('An error occured! ' + error);
+  });
+
+  video.addEventListener('canplay', function(event){
     if (!streaming) {
-      height = video.videoHeight / (video.videoWidth/width);
+      height = video.videoHeight / (video.videoWidth / width);
 
       // Firefox currently has a bug where the height can't be read from
       // the video, so we will make assumptions if this happens.
 
       if (isNaN(height)) {
-        height = width / (4/3);
+        height = width / (4 / 3);
       }
 
       video.setAttribute('width', width);
@@ -141,9 +210,9 @@ function startup() {
     }
   }, false);
 
-  startbutton.addEventListener('click', function(ev){
+  video.addEventListener('click', function(event){
+    event.preventDefault();
     takepicture();
-    ev.preventDefault();
   }, false);
 
   clearphoto();
